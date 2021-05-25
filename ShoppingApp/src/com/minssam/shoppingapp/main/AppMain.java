82a434be90df2923aca48ff86299b3ffd0a2b721
sju01334 <sju01334@gmail.com>
2021-05-25 21:05:50 +0900

    5025

--- ShoppingApp/src/com/minssam/shoppingapp/main/AppMain.java
+++ ShoppingApp/src/com/minssam/shoppingapp/main/AppMain.java
@@ -13,166 +13,193 @@ import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import com.minssam.shoppingapp.config.ConfigMain;
 import com.minssam.shoppingapp.customer.CustomerMain;
+import com.minssam.shoppingapp.member.JoinForm;
 import com.minssam.shoppingapp.member.LoginForm;
 import com.minssam.shoppingapp.member.MemberMain;
 import com.minssam.shoppingapp.order.OrderMain;
 import com.minssam.shoppingapp.product.ProductMain;
 
-public class AppMain extends JFrame implements ActionListener{
-	JPanel p_north;
-	String[] menu_title= {"상품관리","회원관리","주문관리","고객센터","Login","환경설정"};
-	CustomButton[] bt_menu=new CustomButton[menu_title.length]; //배열생성
-	
-	JPanel p_center;//페이지를 교체하기 위한 패널(페이지들 간의 공존을 위해)
-	
-	//페이지 선언 
-	Page[] pages = new Page[6];
-	
-	//데이터베이스 관련
-	String driver="com.mysql.jdbc.Driver";
-	String url="jdbc:mysql://localhost:3306/shoppingapp?characterEncoding=UTF-8";
-	String user="root";
-	String password="1234";
-	private Connection con;
-	
-	public AppMain() {
-		connect();//DB접속
-		
-		//생성
-		p_north = new JPanel();
-		for(int i=0;i< menu_title.length;i++) {
-			bt_menu[i] = new CustomButton(menu_title[i]);
-			bt_menu[i].setId(i); //반복문의 i 를 각 버튼의 식별  id 로 할당!!!
-		}
-		
-		//페이지들 생성 
-		p_center = new JPanel();
-		
-		pages[0] = new ProductMain(this); //상품관리
-		pages[1] = new MemberMain(this);//회원관리
-		pages[2] = new OrderMain(this);//주문관리
-		pages[3] = new CustomerMain(this);//고객센터
-		pages[4] = new LoginForm(this);//로그인
-		pages[5] = new ConfigMain(this);//환경설정
-		
-		//스타일
-		
-		
-		//조립
-		for(JButton bt : bt_menu) { // improved for loop : 주로 집합데이터 형식을 대상으로 한 loop
-			p_north.add(bt);
-		}
-		add(p_north, BorderLayout.NORTH);
-		
-		//p_center 에 페이지들 붙이기
-		for(Page p : pages) {
-			p_center.add(p);
-		}
-		add(p_center);
-		
-		//리스너
-		addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				disConnect(); //DB접속해제
-				System.exit(0); //kill process
-			}
-		});
-		
-		for(int i=0;i<bt_menu.length;i++) {
-			bt_menu[i].addActionListener(this);
-		}
-		
-		//보여주기
-		setBounds(400, 10, 1200, 700);
-		setVisible(true);
-		
+public class AppMain extends JFrame implements ActionListener {
+   JPanel p_north;
+   String[] menu_title = { "상품관리", "회원관리", "주문관리", "고객센터", "Login", "환경설정" };
+   CustomButton[] bt_menu = new CustomButton[menu_title.length]; // 배열생성
 
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		//어떤 버튼이 눌렸는지? - 이벤트가 연결된 컴포넌트를 가리켜 이벤트 소스 
-		Object obj = e.getSource();
-		//obj는 오브젝트 자료형이기 때문에, 버튼을 가리킬수는 있지만, 버튼 보다는 보편적인 기능만을 가지고 
-		//있기에, 즉 가진게 별로 없기에 버튼의 특징을 이용하기 위해서는 버튼 형으로 변환해서 사용하자!!
-		CustomButton bt=(CustomButton)obj; //down casting
-		//System.out.println(bt.getText());
-		
-		//내가 누른 버튼에 해당하는 페이지만 setVisible() 을 true로 놓고 나머지는 false로 놓자!!
-		for(int i=0;i<pages.length;i++) {
-			if(bt.getId()==i) {
-				pages[i].setVisible(true); //현재 선택한 버튼과 같은 인덱스를 갖는 페이지라면..
-			}else {
-				pages[i].setVisible(false);
-			}
-		}
-		
-	}
-	
-	public void connect() {
-		try {
-			Class.forName(driver);
-			con=DriverManager.getConnection(url, user, password);
-			if(con!=null) {
-				this.setTitle("접속성공");
-			}else {
-				this.setTitle("접속실패");
-			}
-		} catch (ClassNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void disConnect() {
-		if(con!=null) {
-			try {
-				con.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	//쿼리문이 DML인 경우
-	public void release(PreparedStatement pstmt) {
-		if(pstmt!=null) {
-			try {
-				pstmt.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	//쿼리문이 Select 인경우
-	public void release(PreparedStatement pstmt, ResultSet rs) {
-		if(rs!=null) {
-			try {
-				rs.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		if(pstmt!=null) {
-			try {
-				pstmt.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	//con getter
-	public Connection getCon() {
-		return con;
-	}
+   JPanel p_center;// 페이지를 교체하기 위한 패널(페이지들 간의 공존을 위해)
 
-	public static void main(String[] args) {
-		new AppMain();
+   // 페이지 선언
+   Page[] pages = new Page[7];
 
-	}
+   // 데이터베이스 관련
+   String driver = "com.mysql.jdbc.Driver";
+   String url = "jdbc:mysql://localhost:3306/shoppingapp?characterEncoding=UTF-8";
+   String user = "root";
+   String password = "1234";
+   private Connection con;
+   private boolean session = false; // 세션이 true 일때 인증 받은 것이고, false 일때는 미인증 간주
+
+   public AppMain() {
+      connect();// DB접속
+
+      // 생성
+      p_north = new JPanel();
+      for (int i = 0; i < menu_title.length; i++) {
+         bt_menu[i] = new CustomButton(menu_title[i]);
+         bt_menu[i].setId(i); // 반복문의 i 를 각 버튼의 식별 id 로 할당!!!
+      }
+
+      // 페이지들 생성
+      p_center = new JPanel();
+
+      pages[0] = new ProductMain(this); // 상품관리
+      pages[1] = new MemberMain(this);// 회원관리
+      pages[2] = new OrderMain(this);// 주문관리
+      pages[3] = new CustomerMain(this);// 고객센터
+      pages[4] = new LoginForm(this);// 로그인
+      pages[5] = new ConfigMain(this);// 환경설정
+      pages[6] = new JoinForm(this);// 회원가입 폼
+
+      // 스타일
+
+      // 조립
+      for (JButton bt : bt_menu) { // improved for loop : 주로 집합데이터 형식을 대상으로 한 loop
+         p_north.add(bt);
+      }
+      add(p_north, BorderLayout.NORTH);
+
+      // p_center 에 페이지들 붙이기
+      for (Page p : pages) {
+         p_center.add(p);
+      }
+      add(p_center);
+
+      // 리스너
+      addWindowListener(new WindowAdapter() {
+         public void windowClosing(WindowEvent e) {
+            disConnect(); // DB접속해제
+            System.exit(0); // kill process
+         }
+      });
+
+      for (int i = 0; i < bt_menu.length; i++) {
+         bt_menu[i].addActionListener(this);
+      }
+
+      // 보여주기
+      //인증여부에 따라 알맞는 페이지 보여주기
+      if(session==false) { //인증받지 않는 상태이므로 디폴트페이지를 로그인페이지로 
+    	  showHide(4);//제일 먼저 보여주고 싶은 페이지
+      }else {
+    	  showHide(0);
+      }
+      
+      setBounds(400, 10, 1200, 700);
+      setVisible(true);
+
+   }
+
+   public void actionPerformed(ActionEvent e) {
+      // 어떤 버튼이 눌렸는지? - 이벤트가 연결된 컴포넌트를 가리켜 이벤트 소스
+      Object obj = e.getSource();
+      // obj는 오브젝트 자료형이기 때문에, 버튼을 가리킬수는 있지만, 버튼 보다는 보편적인 기능만을 가지고
+      // 있기에, 즉 가진게 별로 없기에 버튼의 특징을 이용하기 위해서는 버튼 형으로 변환해서 사용하자!!
+      CustomButton bt = (CustomButton) obj; // down casting
+      // System.out.println(bt.getText());
+
+      if(session) {
+    	  showHide(bt.getId());
+      }else {
+    	  JOptionPane.showMessageDialog(this, "로그인이 필요한 서비스 입니다");
+      }
+   }
+
+   public void connect() {
+      try {
+         Class.forName(driver);
+         con = DriverManager.getConnection(url, user, password);
+         if (con != null) {
+            this.setTitle("접속성공");
+         } else {
+            this.setTitle("접속실패");
+         }
+      } catch (ClassNotFoundException e1) {
+         e1.printStackTrace();
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+   }
+
+   public void disConnect() {
+      if (con != null) {
+         try {
+            con.close();
+         } catch (SQLException e) {
+            e.printStackTrace();
+         }
+      }
+   }
+
+   // 쿼리문이 DML인 경우
+   public void release(PreparedStatement pstmt) {
+      if (pstmt != null) {
+         try {
+            pstmt.close();
+         } catch (SQLException e) {
+            e.printStackTrace();
+         }
+      }
+   }
+
+   // 쿼리문이 Select 인경우
+   public void release(PreparedStatement pstmt, ResultSet rs) {
+      if (rs != null) {
+         try {
+            rs.close();
+         } catch (SQLException e) {
+            e.printStackTrace();
+         }
+      }
+      if (pstmt != null) {
+         try {
+            pstmt.close();
+         } catch (SQLException e) {
+            e.printStackTrace();
+         }
+      }
+   }
+
+   // con getter
+   public Connection getCon() {
+      return con;
+   }
+
+   public void showHide(int n) { //보여주고 싶은 페이지의 변호를 넘기면 된다..
+      // 내가 누른 버튼에 해당하는 페이지만 setVisible() 을 true로 놓고 나머지는 false로 놓자!!
+      for (int i = 0; i < pages.length; i++) {
+         if (n == i) {
+            pages[i].setVisible(true); // 현재 선택한 버튼과 같은 인덱스를 갖는 페이지라면..
+         } else {
+            pages[i].setVisible(false);
+         }
+      }
+   }
+
+   
+   public boolean isSession() {
+	   return session;
+   }
+
+   public void setSession(boolean session) {
+	   this.session = session;
+   }
+
+public static void main(String[] args) {
+      new AppMain();
+
+   }
 
 }
--- ShoppingApp/src/com/minssam/shoppingapp/product/ProductMain.java
+++ ShoppingApp/src/com/minssam/shoppingapp/product/ProductMain.java
@@ -12,6 +12,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -84,8 +86,8 @@ public class ProductMain extends Page{
 	
 	//동쪽관련
 	JPanel p_east;
-	Choice ch_top2;
-	Choice ch_sub2;
+	JTextField t_top;
+	JTextField t_sub;
 	JTextField t_product_name2;
 	JTextField t_price2;
 	JTextField t_brand2;
@@ -94,15 +96,18 @@ public class ProductMain extends Page{
 	JButton bt_web2; //웹에서 가져오기
 	JButton bt_file2; //로컬 파일에서 가져오기
 	Canvas can2;
-	JButton bt_regist2;
+	JButton bt_del;
 	
 	JFileChooser chooser;
 	Toolkit kit=Toolkit.getDefaultToolkit();
 	Image image; //등록시 미리보기에 사용할 이미지
+	Image image2; //상세보기 사용할 이미지
 	String filename;//유저의 복사에 의해 싱성된 파일명
 	
 	String[] columns= {"product_id","subcategory_id","product_name","price","brand","detail","filename"};//컬럼 
 	String[][] records= {};
+	int product_id; //현재 상세보기중인 product의 pk
+	String del_file; //현재 상세보기 중인 filename (삭제 대상이 될 파일)
 
 	
 /*========================생성자============================*/
@@ -152,21 +157,33 @@ public class ProductMain extends Page{
 			public int getColumnCount() {
 				return columns.length;
 			}
-			@Override
 			public String getColumnName(int col) {
 				return columns[col];
 			}
-			
 			public Object getValueAt(int row, int col) {
 				return records[row][col];
+			}
+			//Jtable의 각 셀의 값을 지정
+			//셀을 편집할 후, 엔터치는 순간 아래의 메서드 호출됨
+			public void setValueAt(Object value, int row, int col) {
+				System.out.println( row+","+col+" 번째 셀의 데이터는 "+value+"로 바꿀게요");
+				records[row][col]=(String)value;
+				updateProduct();
+			}
+			public boolean isCellEditable(int row, int col) {
+				if(col==0) {//첫번째 열인 product_id만 읽기전용으로 세팅
+					return false;
+				}else {
+					return true;
+				}
 			}
 		});
 		scroll_table = new JScrollPane(table);
 		
 		//동쪽 영역 생성 
 		p_east= new JPanel();
-		ch_top2 = new Choice();
-		ch_sub2 = new Choice();
+		t_top = new JTextField();
+		t_sub = new JTextField();
 		t_product_name2 = new JTextField();
 		t_price2 = new JTextField();
 		t_brand2 = new JTextField();
@@ -174,8 +191,12 @@ public class ProductMain extends Page{
 		scroll2 = new JScrollPane(t_detail2);
 		bt_web2 = new JButton("웹에서찾기");
 		bt_file2 = new JButton("파일찾기");
-		can2 = new Canvas();
-		bt_regist2 = new JButton("상품등록");
+		can2 = new Canvas() {
+			public void paint(Graphics g) {
+				g.drawImage(image2, 0, 0, 180, 180, can2);
+			}
+		};
+		bt_del = new JButton("상품삭제");
 		
 		chooser=new JFileChooser("C:\\koreaIT\\jsworkspace\\images");
 		
@@ -208,12 +229,14 @@ public class ProductMain extends Page{
 		
 		//동쪽관련
 		p_east.setPreferredSize(new Dimension(200, 700));
-		scroll2.setPreferredSize(new Dimension(180, 300));
-		ch_top2.setPreferredSize(d);
-		ch_sub2.setPreferredSize(d);
+		scroll2.setPreferredSize(new Dimension(180, 180));
+		t_top.setPreferredSize(d);
+		t_sub.setPreferredSize(d);
 		t_product_name2.setPreferredSize(d);
 		t_price2.setPreferredSize(d);
 		t_brand2.setPreferredSize(d);
+		can2.setPreferredSize(new Dimension(180, 180));
+		can2.setBackground(Color.YELLOW);
 		
 		/*----------------------------------------------------[조립]*/
 		//서쪽조립 
@@ -229,8 +252,8 @@ public class ProductMain extends Page{
 		p_west.add(bt_regist);
 		
 		//동쪽조립
-		p_east.add(ch_top2);
-		p_east.add(ch_sub2);
+		p_east.add(t_top);
+		p_east.add(t_sub);
 		p_east.add(t_product_name2);
 		p_east.add(t_price2);
 		p_east.add(t_brand2);
@@ -238,7 +261,7 @@ public class ProductMain extends Page{
 		p_east.add(bt_web2);
 		p_east.add(bt_file2);
 		p_east.add(can2);
-		p_east.add(bt_regist2);
+		p_east.add(bt_del);
 		
 		add(p_west, BorderLayout.WEST);//서쪽영역에 부착 
 		add(p_east, BorderLayout.EAST);//서쪽영역에 부착 
@@ -285,16 +308,35 @@ public class ProductMain extends Page{
 			}
 		});
 		
+		//등록버튼 리스너 연결
 		bt_regist.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				regist();
-				getProductList();
+				//유효성 체크 통과되면 아래의 두 매서드 호출
+				//숫자값을 문자로 입력할 경우 문제가 심각.. 따라서 이부분만 체크해보자
+				try {
+					Integer.parseInt(t_price.getText()); // "88888" 
+					regist();
+					getProductList();
+					
+				}catch(NumberFormatException nfe){
+					JOptionPane.showMessageDialog(ProductMain.this.getAppMain(), "숫자를 입력하세요");
+					t_price.setText(""); //기존 입력값 지우고
+					t_price.requestFocus();//포커스 올려놓기
+				}
+				
+				//예외는 에러가 발생할 가능성이 있는 우려되는 코드에 대한 안전장치인데
+				//지금까지는 컴파일 타임에 즉 컴파일러에 의해 무조건 처리가 강요되는 예외만 사용해왔으나
+				//이제부터는 컴파일 타임이 아닌 실행시 즉 런타임시에 관여하는 예외도 있다
+				//이러한 예외를 런타임예외라 하며, RuntimeException 클래스로부터 상속받은 자식들이다.
+				//강요하지 않은 예외는 개발자의 선택에 의해 처리 여부를 결정하면 된다.
+				//대표적인 RuntimeException ArrayIndexOutOfBoundsException
 				
 			}
 		});
 		
+		//엑섹등록버튼과 리스너 연결
 		bt_excel.addActionListener(new ActionListener() {
 			
 			@Override
@@ -302,6 +344,44 @@ public class ProductMain extends Page{
 				registByExcel();
 			}
 		});
+		
+		//검색버튼과 리스너 연결
+		bt_search.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//검색을 안할 경우 모든 데이터가 나오게 (category선택 안하고, Keyword입력x)
+				if(ch_category.getSelectedIndex()==0 && t_keyword.getText().length()==0) {
+					getProductList();
+				}else {
+					//검색 결과만 나오게
+					getListBySearch();
+				}
+				
+			}
+		});
+		
+		//테이블과 리스너 연결
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				getDetail();
+			}
+	
+		});
+		
+		//상품삭제와 리스너 연결
+		bt_del.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(JOptionPane.showConfirmDialog(ProductMain.this.getAppMain(), "삭제하시겠어요?")==JOptionPane.OK_OPTION) {
+					deleteProduct();
+				}
+			}
+		});
+		
+		
 		
 		getTopList();
 		getProductList();
@@ -596,7 +676,15 @@ public class ProductMain extends Page{
 		FileInputStream fis=null;
 		XSSFWorkbook workbook=null;
 		PreparedStatement  pstmt=null;
+		
+		Connection con=this.getAppMain().getCon();
+//		con.setAutoCommit(false);//자동커밋하지마라~~ 커밋은 내가 주도해서 할거야
+//		con.setAutoCommit(true);//매 DML 마다 무조건 커밋해라
+		
+		
 		try {
+			con.setAutoCommit(false);
+			
 			fis=new FileInputStream(path);
 			//이 스트림을 통해 내부데이터를 엑셀로 이해할수 있도록 해석
 			//액셀파일을 처리하기 위한 객체 XSSFWorkbook
@@ -644,9 +732,7 @@ public class ProductMain extends Page{
 				String sql="insert into product(subcategory_id, product_name, price, brand, detail, filename)";
 				sql+=" values(?,?,?,?,?,?)";
 				
-				Connection con=this.getAppMain().getCon();
-//				con.setAutoCommit(false);//자동커밋하지마라~~ 커밋은 내가 주도해서 할거야
-//				con.setAutoCommit(true);//매 DML 마다 커밋해라
+				
 				
 				pstmt=null;
 				pstmt=this.getAppMain().getCon().prepareStatement(sql);
@@ -661,6 +747,7 @@ public class ProductMain extends Page{
 				pstmt.executeUpdate();
 			}
 
+			con.commit();//트랜잭션 확정
 			JOptionPane.showMessageDialog(this.getAppMain(), "등록완료");
 			getProductList();
 			
@@ -671,6 +758,13 @@ public class ProductMain extends Page{
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
+			//이 영역이 만일DML실패에 의한 에러를 만난 이유로실행된다면 rollback
+			try {
+				con.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			
 		}finally {
 			if(fis!=null) {
 				try {
@@ -682,11 +776,164 @@ public class ProductMain extends Page{
 			if(pstmt!=null) {
 				this.getAppMain().release(pstmt);
 			}
+			
+			try {
+				con.setAutoCommit(true);//다시 돌려놓기
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
+	//검색결과 가져오기
+	public void getListBySearch() {
+		String category=ch_category.getSelectedItem();
+		String keyword=t_keyword.getText();
+		
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
+		String sql="select product_id, sub_name, product_name, price, brand, detail,filename";
+		sql+=" from subcategory s, product p";
+		sql+=" where s.subcategory_id=p.subcategory_id and "+category+" like '%"+keyword+"%'";
+		
+//		System.out.println(sql);
+		
+		try {
+			pstmt=this.getAppMain().getCon().prepareStatement(sql
+					, ResultSet.TYPE_SCROLL_SENSITIVE
+					, ResultSet.CONCUR_READ_ONLY);
+			rs=pstmt.executeQuery();
+			rs.last(); //커서를 마지막레코드로 보냄
+			int total=rs.getRow();
+			
+			//JTable 이 참조하고 있는 records라는 이차원배열의 값을, rs를 이용하여 갱신해보자
+			records=new String[total][columns.length];
+			
+			rs.beforeFirst(); //커서 위치 제자리로
+			int index=0;
+			while(rs.next()) {
+				records[index][0]=Integer.toString(rs.getInt("product_id"));
+				records[index][1]=rs.getString("sub_name");
+				records[index][2]=rs.getString("product_name");
+				records[index][3]=Integer.toString(rs.getInt("price"));
+				records[index][4]=rs.getString("brand");
+				records[index][5]=rs.getString("detail");
+				records[index][6]=rs.getString("filename");
+				index++;
+			}
+			table.updateUI();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			this.getAppMain().release(pstmt, rs);
+		}
+		
+	}
 	
+	//상세보기 구현 
+	public void getDetail() {
+		//선택한 레코드의 product_id
+		product_id=Integer.parseInt((String)table.getValueAt(table.getSelectedRow(), 0));
+		
+		//String immuable 특징이 있기 때문에, 즉 문자열 상수이기에 아래와 같이 sql문을 처리하면 
+		//문자열상수가 5개가 생성된다, 즉 sql이 수정되는게 아니다!!!
+		//따라서 좀더 메모리 효율을 생각한다면, 수정가능한 문자열처리를 해야 한다 
+		StringBuffer sb = new StringBuffer();
+		
+		sb.append("select product_id,top_name, sub_name, product_name, price, brand, detail,filename");
+		sb.append(" from topcategory t, subcategory s, product p");
+		sb.append(" where t.topcategory_id=s.topcategory_id and");
+		sb.append(" s.subcategory_id = p.subcategory_id and ");
+		sb.append(" product_id="+product_id);
+		
+//		System.out.println(sb.toString());
+		
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
+		try {
+			pstmt=this.getAppMain().getCon().prepareStatement(sb.toString());
+			rs=pstmt.executeQuery(); //select 실행 후 결과 받기 !!!
+			
+			if(rs.next()) { //레코드가 있다면..
+				//우측 영역에 채워넣기!!!
+				t_top.setText(rs.getString("top_name"));
+				t_sub.setText(rs.getString("sub_name"));
+				t_product_name2.setText(rs.getString("product_name"));
+				t_price2.setText(Integer.toString(rs.getInt("price")));
+				t_brand2.setText(rs.getString("brand"));
+				t_detail2.setText(rs.getString("detail"));
+				del_file= rs.getString("filename");
+				
+				//우측 켄버스에 이미지 나오게!!!
+				image2=kit.getImage("C:\\koreaIT\\javaworkspace\\ShoppingApp\\data\\"+rs.getString("filename"));
+				can2.repaint();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			this.getAppMain().release(pstmt, rs);
+		}
+	}
 	
+	//상품 삭제 처리 
+	public void deleteProduct() {
+		//데이터 삭제 + 파일삭제 
+		String sql="delete from product where product_id="+product_id;
+		PreparedStatement pstmt=null;
+		
+		try {
+			pstmt=this.getAppMain().getCon().prepareStatement(sql);
+			int result=pstmt.executeUpdate(); //DML중 delete 수행
+			if(result >0) {
+				//파일 삭제!!
+				File file=new File("C:\\koreaIT\\javaworkspace\\ShoppingApp\\data\\"+del_file);
+				file.delete(); //파일 삭제
+				getProductList(); //리스트 다시 조회
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
-	
+	//상품 한건 수정
+	public void updateProduct() {
+		System.out.println("수정할 product_id는"+product_id);
+		
+		String sql="update product set product_name=?, price=?, brand=?, detail=?, filename=?";
+		sql+=" where product_id=?";
+		
+		PreparedStatement pstmt=null;
+		
+		try {
+			pstmt=this.getAppMain().getCon().prepareStatement(sql);
+			
+			String product_name=(String)table.getValueAt(table.getSelectedRow(), 2);
+			int price=Integer.parseInt((String)table.getValueAt(table.getSelectedRow(), 3));
+			String brand=(String)table.getValueAt(table.getSelectedRow(), 4);
+			String detail=(String)table.getValueAt(table.getSelectedRow(), 5);
+			String filename=(String)table.getValueAt(table.getSelectedRow(), 6);
+			
+			pstmt.setString(1, product_name);//product_name
+			pstmt.setInt(2, price);
+			pstmt.setString(3, brand);//brand
+			pstmt.setString(4, detail);//detail
+			pstmt.setString(5, filename);//filename
+			pstmt.setInt(6, product_id);//product_id
+			
+			int result=pstmt.executeUpdate();
+			if(result>0) {
+				JOptionPane.showMessageDialog(this.getAppMain(), "수정완료");
+			}else {
+				JOptionPane.showMessageDialog(this.getAppMain(), "수정실패");
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			this.getAppMain().release(pstmt);
+		}
+	}
 }
